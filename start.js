a4b18c0146136624796db68bdcecb9328607d1e9
Brian Zelip <bzelip@gmail.com>
2017-12-31 18:59:13 -0500

    add start.js

---
+++ start.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: 'variables.env' });
+
+// Connect to our Database and handle any bad connections
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', err => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+// import all of our models (data schemas)
+// this is also known as a "singleton" - where express allows you to just require it once at the beginning, and all subsequent files get it from here.
+// require('./models/Store');
+
+// Start the app!
+const app = require('./app');
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
