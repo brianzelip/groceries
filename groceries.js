65dca3c515461083799537305b9707b31b85a254
Brian Zelip <bzelip@gmail.com>
2017-12-31 18:59:25 -0500

    add groceries.js data

---
+++ groceries.js
@@ -0,0 +1,76 @@
+// Trader Joe's items we regularly purchase,
+// and the order of item as found throughout the Towson, MD store.
+
+let tjs = [
+  { name: 'onions', types: ['white', 'red', 'sweet'], area: 1 },
+  { name: 'garlic', area: 1 },
+  { name: 'dried fruit', types: ['cherries', 'mango'], area: 1 },
+  { name: 'nuts', types: ['raw almonds', 'raw walnuts'], area: 1 },
+  { name: 'peppers', types: ['green', 'red', 'yellow'], area: 1 },
+  { name: 'potatoes', types: ['red', 'white'], area: 1 },
+  { name: 'cucumbers', area: 1 },
+  { name: 'kale', area: 1 },
+  { name: 'apples', area: 1 },
+  { name: 'hummus', area: 2 },
+  { name: 'tahini dip', area: 2 },
+  { name: 'eggs', area: 2 },
+  { name: 'frozen pizza', area: 3 },
+  { name: 'frozen salmon', area: 3 },
+  {
+    name: 'crackers',
+    types: ['thin mini', 'thin mini tamari', 'water'],
+    area: 4
+  },
+  { name: 'cereal', types: ['bran', 'raisin bran'], area: 4 },
+  { name: 'chips', types: ['tortilla', 'potatoe'], area: 4 },
+  { name: 'orange juice', area: 4 },
+  { name: 'cream cheese', area: 4 },
+  { name: 'sour cream', area: 4 },
+  { name: 'greek yogurt', area: 4 },
+  { name: 'milk', types: ['whole', 'soy'], area: 5 },
+  { name: 'cream', types: ['half &amp; half', 'heavy'], area: 5 },
+  { name: 'dog food', area: 6 },
+  { name: 'dog biscuits', area: 6 },
+  { name: 'tissues', area: 6 },
+  { name: 'tooth paste', area: 6 },
+  { name: 'sponges', area: 6 },
+  { name: 'tahini sauce', area: 6 },
+  { name: 'maple syrup', area: 6 },
+  { name: 'pasta', area: 6 },
+  { name: 'pasta sauce', area: 6 },
+  { name: 'olive oil', area: 6 },
+  { name: 'pickles', area: 6 },
+  {
+    name: 'vinegar',
+    types: ['balsalmic', 'white', 'apple cider', 'red wine'],
+    area: 6
+  },
+  { name: 'flour', area: 6 },
+  { name: 'mustard', types: ['brown', 'yellow', 'seed'], area: 6 },
+  {
+    name: 'bread',
+    types: ['sprouted rye', 'sprouted whole wheat', 'sour'],
+    area: 7
+  },
+  { name: 'soda water', area: 7 },
+  {
+    name: 'chicken',
+    types: ['whole', 'half', 'breasts', 'boneless skinless breasts'],
+    area: 7
+  },
+  { name: 'pork chops', area: 7 },
+  { name: 'steak', area: 7 },
+  { name: 'ground beef', area: 7 },
+  {
+    name: 'cheese',
+    types: [
+      'swiss',
+      'monterey jack',
+      'mozerella wedge',
+      'mozerella fresh',
+      'bleu cheese',
+      'gorgonzola'
+    ],
+    area: 7
+  }
+];
