3512735d86ef97a148f9f080d8e6f7ecb859b389
Brian Zelip <bzelip@gmail.com>
2018-01-01 15:01:22 -0500

    add helpers.js

---
+++ helpers.js
@@ -0,0 +1,21 @@
+/*
+  This is a file of data and helper functions that we can expose and use in our templating function
+*/
+
+// FS is a built in module to node that let's us read files from the system we're running on
+const fs = require('fs');
+
+// Dump is a handy debugging function we can use to sort of "console.log" our data
+exports.dump = obj => JSON.stringify(obj, null, 2);
+
+// Some details about the site
+exports.siteDescription = `Family groceries web app.`;
+exports.siteKeywords = ['Node.js', 'Express.js', 'MongoDB', 'groceries'];
+
+exports.menu = [
+  { slug: '/stores', title: 'Stores', icon: 'store' },
+  { slug: '/tags', title: 'Tags', icon: 'tag' },
+  { slug: '/top', title: 'Top', icon: 'top' },
+  { slug: '/add', title: 'Add', icon: 'add' },
+  { slug: '/map', title: 'Map', icon: 'map' }
+];
