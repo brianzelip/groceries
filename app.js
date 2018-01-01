9037e70b5273969b3c1dfc6644c2a646773c1b93
Brian Zelip <bzelip@gmail.com>
2018-01-01 15:03:51 -0500

    update app.js with helpers and use bodyParser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
+const helpers = require('./helpers');
 
 // create our Express app
 const app = express();
@@ -13,6 +14,19 @@ app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work gr
 
 // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Takes the raw requests and turns them into usable properties on req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// pass variables to our templates + all requests
+app.use((req, res, next) => {
+  res.locals.h = helpers;
+  // res.locals.flashes = req.flash();
+  // res.locals.user = req.user || null;
+  // res.locals.currentPath = req.path;
+  next();
+});
 
 app.use('/', routes);
 
