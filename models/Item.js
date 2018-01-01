29767bb96eaeb82ffc2e5fd55c4ae42ff07539bf
Brian Zelip <bzelip@gmail.com>
2018-01-01 14:19:46 -0500

    rename model to Item

--- models/Item.js
+++ models/Item.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 // have to tell mongoose that we're using ES6 async/await
 mongoose.Promise = global.Promise;
+const slug = require('slug');
 
-const groceryItemSchema = new mongoose.Schema({
+const itemSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true
   },
-  type: {
+  variant: {
     type: String,
     trim: true
   },
-  area: Number
+  area: Number,
+  slug: String
 });
 
-module.exports = mongoose.model('GroceryItem', groceryItemSchema);
+module.exports = mongoose.model('Item', itemSchema);
