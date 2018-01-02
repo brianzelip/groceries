38dda72dbefef5ca2b1a2cdcda1c3b7238b85496
Brian Zelip <bzelip@gmail.com>
2018-01-01 19:36:50 -0500

    wire up flashes in app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const path = require('path');
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
 const routes = require('./routes/routes');
 const helpers = require('./helpers');
 const errorHandlers = require('./handlers/errorHandlers');
@@ -20,10 +23,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Sessions allow us to store data on visitors from request to request
+// This keeps users logged in and allows us to send flash messages
+app.use(
+  session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
+
+// // The flash middleware let's us use req.flash('error', 'Shit!'), which will then pass that message to the next page the user requests
+app.use(flash());
+
 // pass variables to our templates + all requests
 app.use((req, res, next) => {
   res.locals.h = helpers;
-  // res.locals.flashes = req.flash();
+  res.locals.flashes = req.flash();
   // res.locals.user = req.user || null;
   // res.locals.currentPath = req.path;
   next();
