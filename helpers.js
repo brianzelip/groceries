76087b2f61a2d7692dd8d2a43e929b6178b32c2f
Brian Zelip <bzelip@gmail.com>
2018-01-21 14:45:40 -0500

    add h.icon() to helpers from learn-node

--- helpers.js
+++ helpers.js
@@ -8,6 +8,9 @@ const fs = require('fs');
 // Dump is a handy debugging function we can use to sort of "console.log" our data
 exports.dump = obj => JSON.stringify(obj, null, 2);
 
+// inserting an SVG
+exports.icon = name => fs.readFileSync(`./public/icons/${name}.svg`);
+
 // Some details about the site
 exports.site = {
   name: 'groceries',
